01be9b36804108cf2c43b492e967fd0d506bdd8d
pcssc <johnfiles@Johns-MacBook-Pro.local>
2019-03-02 05:20:22 +1100

    urimi

--- js/sub-system.js
+++ js/sub-system.js
@@ -1,6 +1,8 @@
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
+
 function toggle() {
   var a = document.getElementById("style");
-  a.x = '../../css/styleEzeze' == a.x ? '../../css/style' : '../../css/styleEzeze'; // short if
+  a.x = '../../../css/styleEzeze' == a.x ? '../../../css/style' : '../../../css/styleEzeze'; // short if
   a.href = a.x + '.css';
 }
 
@@ -12,3 +14,17 @@ function debugMode() {
     debugModev.style.display = "none";
   }
 }
+
+// var url="schoolname";
+// var extra="/Name-Name-Class";
+// document.getElementById('usersys').src = url+extra;
+
+function engage() {
+  var  bforinfil = "users";
+  var  htminfil = "index.html";
+  var  infil = "/";
+  document.getElementById("idlo").value;
+  document.getElementById("idlous").value;
+  window.location.href = bforinfil + infil + document.getElementById("idlo").value + infil + document.getElementById("idlous").value + infil+ htminfil;
+}
+document.getElementById("timelang").style.color = "#" + randomColor;
